f7a1b7d09269e70e04fb8799bf71898bcae533e0
Chetan <chetan882777@gmail.com>
2021-07-09 05:50:03 +0530

    register data

--- lib/modules/register/RegisterScreen.dart
+++ lib/modules/register/RegisterScreen.dart
@@ -28,7 +28,7 @@ class RegisterScreen extends StatelessWidget {
         mapper: mapper,
         saveController: saveVariable);
     return ChangeNotifierProvider<RegisterViewModel>(
-      create: (context) => RegisterViewModel(context),
+      create: (context) => RegisterViewModel(context, matForms),
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
@@ -48,109 +48,124 @@ class RegisterScreen extends StatelessWidget {
             Padding(
               padding: EdgeInsets.only(left: 32, right: 32, top: 8),
               child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                        height: 100,
-                        width: double.infinity,
-                        child: Stack(
-                          children: [
-                            Align(
-                                child: Container(
-                              height: 100,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(100),
-                                  border: Border.all(
-                                      width: 2,
-                                      color: Theme.of(context).primaryColor)),
-                              child: Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: Icon(
-                                    Icons.person,
-                                    size: 80,
-                                    color: Theme.of(context).primaryColor,
-                                  )),
-                            )),
-                            Align(
-                              child: InkWell(
-                                onTap: () {
-
-                                },
-                                child: Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: Icon(
-                                    Icons.edit,
-                                    color: Theme.of(context).primaryColor,
+                child: FormBuilder(
+                  key: dynamicFormKey,
+                  autovalidateMode: AutovalidateMode.disabled,
+                  child: Consumer(
+                    builder: (context, RegisterViewModel model, child) =>
+                        Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                            height: 100,
+                            width: double.infinity,
+                            child: Stack(
+                              children: [
+                                Align(
+                                    child: Container(
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(100),
+                                      border: Border.all(
+                                          width: 2,
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  child: Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Icon(
+                                        Icons.person,
+                                        size: 80,
+                                        color: Theme.of(context).primaryColor,
+                                      )),
+                                )),
+                                Align(
+                                  child: InkWell(
+                                    onTap: () {},
+                                    child: Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Icon(
+                                        Icons.edit,
+                                        color: Theme.of(context).primaryColor,
+                                      ),
+                                    ),
                                   ),
+                                  alignment: Alignment(0.65, 0.0),
                                 ),
-                              ),
-                              alignment: Alignment(0.65, 0.0),
-                            ),
-                          ],
-                        )),
-                    matForms.matEditable(
-                      variable: "first_name",
-                      displayText: "First Name",
-                      player: (val) {},
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(context),
-                        FormBuilderValidators.max(context, 70),
-                      ]),
+                              ],
+                            )),
+                        matForms.matEditable(
+                          variable: "firstName",
+                          displayText: "First Name",
+                          textInputType: TextInputType.name,
+                          player: (val) {},
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(context),
+                            FormBuilderValidators.max(context, 70),
+                          ]),
+                        ),
+                        matForms.matEditable(
+                          variable: "lastName",
+                          displayText: "Last Name",
+                          textInputType: TextInputType.name,
+                          player: (val) {},
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(context),
+                            FormBuilderValidators.max(context, 70),
+                          ]),
+                        ),
+                        matForms.matEditable(
+                          variable: "email",
+                          displayText: "Email Address",
+                          textInputType: TextInputType.emailAddress,
+                          player: (val) {},
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(context),
+                            FormBuilderValidators.max(context, 70),
+                          ]),
+                        ),
+                        matForms.matEditable(
+                          variable: "contact",
+                          displayText: "Contact",
+                          textInputType: TextInputType.phone,
+                          player: (val) {},
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(context),
+                          ]),
+                        ),
+                        matForms.matEditable(
+                          variable: "department",
+                          displayText: "Department",
+                          textInputType: TextInputType.name,
+                          player: (val) {},
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(context),
+                            FormBuilderValidators.max(context, 70),
+                          ]),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 70.0,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                            child: matForms.elevatedBtn(
+                                color: Theme.of(context).accentColor,
+                                textColor: Colors.white,
+                                displayText: "Submit",
+                                player: () {
+                                  dynamicFormKey.currentState!
+                                      .saveAndValidate();
+                                  model.submit();
+                                }),
+                          ),
+                        ),
+                      ],
                     ),
-                    matForms.matEditable(
-                      variable: "last_name",
-                      displayText: "Last Name",
-                      player: (val) {},
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(context),
-                        FormBuilderValidators.max(context, 70),
-                      ]),
-                    ),
-                    matForms.matEditable(
-                      variable: "email_address",
-                      displayText: "Email Address",
-                      player: (val) {},
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(context),
-                        FormBuilderValidators.max(context, 70),
-                      ]),
-                    ),
-                    matForms.matEditable(
-                      variable: "contact",
-                      displayText: "Contact",
-                      player: (val) {},
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(context),
-                        FormBuilderValidators.max(context, 70),
-                      ]),
-                    ),
-                    matForms.matEditable(
-                      variable: "department",
-                      displayText: "Department",
-                      player: (val) {},
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(context),
-                        FormBuilderValidators.max(context, 70),
-                      ]),
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 70.0,
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                        child: matForms.elevatedBtn(
-                            color: Theme.of(context).accentColor,
-                            textColor: Colors.white,
-                            displayText: "Submit",
-                            player: () {
-                              print(" ==== Submit");
-                            }),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
--- lib/modules/register/RegisterViewModel.dart
+++ lib/modules/register/RegisterViewModel.dart
@@ -1,7 +1,21 @@
 import 'package:agro_worlds/modules/BaseViewModel.dart';
+import 'package:agro_worlds/utils/builders/MATForms.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class RegisterViewModel extends BaseViewModel {
-  RegisterViewModel(BuildContext context) : super(context);
 
+  MATForms matForms;
+
+  RegisterViewModel(BuildContext context, this.matForms) : super(context);
+
+  void submit() {
+    if(matForms.dynamicFormKey.currentState != null) {
+      var formData = matForms.dynamicFormKey.currentState!.value;
+      var reqData = Map();
+      formData.forEach((key, value) {
+        reqData[key] = value;
+      });
+      reqData.putIfAbsent("image", () => "");
+    }
+  }
 }
